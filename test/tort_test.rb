ca08cd500dfd7e4aa3e6a917e98e74340001ceb2
Nathan Klapstein <nklapste@ualberta.ca>
2019-03-05 16:26:22 -0700

    improving test readability

--- test/tort_test.rb
+++ test/tort_test.rb
@@ -10,12 +10,12 @@ class TortTest < Test::Unit::TestCase
   end
 
   def test_tort_thread_sort
-    unsorted_array = [2, 1, 3]
+    unsorted_array = [-3, 0, 2, 1, 3, -1, -2]
     assert_equal(unsorted_array.sort, Tort.tort_thread_sort(unsorted_array))
   end
 
   def test_tort_thread_sort_rand
-    unsorted_array = Array.new(10_000) { rand(-10_000_000...10_000_000) }
+    unsorted_array = Array.new(10) { rand(-10_000_000...10_000_000) }
     assert_equal(unsorted_array.sort, Tort.tort_thread_sort(unsorted_array))
   end
 
@@ -25,7 +25,7 @@ class TortTest < Test::Unit::TestCase
   end
 
   def test_tort_process_sort_rand
-    unsorted_array = Array.new(10_000) { rand(-10_000_000...10_000_000) }
+    unsorted_array = Array.new(10) { rand(-10_000_000...10_000_000) }
     assert_equal(unsorted_array.sort, Tort.tort_process_sort(unsorted_array))
   end
 end
