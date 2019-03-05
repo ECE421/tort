339a90b410bb71db9a592f5ea1730af74872c36a
Nathan Klapstein <nklapste@ualberta.ca>
2019-03-05 12:00:09 -0700

    adding proto benchmark

--- test/tort_test.rb
+++ test/tort_test.rb
@@ -1,5 +1,22 @@
 require_relative 'helper'
 
+# Compare runtimes between tort's sort and a standard Ruby **unoptimized** sort
+def tort_sort_faster?(unsorted_array)
+  # multi-threaded sort
+  sorter = Tort.new
+  start = Time.now
+  sorter.tort_sort(unsorted_array, &:<=>)
+  finish = Time.now
+  tort_time = finish - start
+
+  # normal sort
+  start = Time.now
+  unsorted_array.sort(&:<=>)
+  finish = Time.now
+  normal_unoptimized_time = finish - start
+  tort_time < normal_unoptimized_time
+end
+
 class TortTest < Test::Unit::TestCase
   def setup
     # Do nothing
@@ -26,22 +43,13 @@ class TortTest < Test::Unit::TestCase
     assert_equal(unsorted_array.sort, sorter.tort_sort(unsorted_array))
   end
 
+  def benchmark_tort_sort_small
+    unsorted_array = Array.new(10_000) { rand(1...10_000_000) }
+    assert_false(tort_sort_faster?(unsorted_array))
+  end
+
   def benchmark_tort_sort_large
     unsorted_array = Array.new(10_000_000) { rand(1...10_000_000) }
-
-    # multi-threaded sort
-    sorter = Tort.new
-    start = Time.now
-    sorter.tort_sort(unsorted_array, &:<=>)
-    finish = Time.now
-    tort_time = finish - start
-
-    # normal sort
-    start = Time.now
-    unsorted_array.sort(&:<=>)
-    finish = Time.now
-    normal_unoptimized_time = finish - start
-
-    assert_true(tort_time < normal_unoptimized_time)
+    assert_true(tort_sort_faster?(unsorted_array))
   end
 end
