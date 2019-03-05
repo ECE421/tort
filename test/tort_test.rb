ff93b0b965f5cb8853fc5beb0ab724b0c164686e
Nathan Klapstein <nklapste@ualberta.ca>
2019-03-05 16:38:38 -0700

    cleaning up tests

--- test/tort_test.rb
+++ test/tort_test.rb
@@ -9,19 +9,9 @@ class TortTest < Test::Unit::TestCase
     # Do nothing
   end
 
-  def test_tort_thread_sort
-    unsorted_array = [-3, 0, 2, 1, 3, -1, -2]
-    assert_equal(unsorted_array.sort, Tort.tort_thread_sort(unsorted_array))
-  end
-
   def test_tort_thread_sort_rand
     unsorted_array = Array.new(10) { rand(-10_000_000...10_000_000) }
     assert_equal(unsorted_array.sort, Tort.tort_thread_sort(unsorted_array))
-  end
-
-  def test_tort_process_sort
-    unsorted_array = [-3, 0, 2, 1, 3, -1, -2]
-    assert_equal(unsorted_array.sort, Tort.tort_process_sort(unsorted_array))
   end
 
   def test_tort_process_sort_rand
