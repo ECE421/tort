8eb30405b9810844e42c1b9641e4466d1d52e093
Nathan Klapstein <nklapste@ualberta.ca>
2019-03-06 09:07:15 -0700

    improving commenting

--- lib/tort.rb
+++ lib/tort.rb
@@ -1,25 +1,30 @@
 require 'parallel'
 
-# Multi-threaded / multi-processed sort for Ruby
+# Multi-threaded / multi-processed sorting for Ruby
 class Tort
+  # Sort an array utilizing concurrency via multiple processes.
   def self.tort_thread_sort(unsorted_array, thread_workers = Parallel.processor_count, &block)
     Parallel.map(chunk_array(unsorted_array, thread_workers), in_threads: thread_workers) do |sub_array|
       sub_array.sort(&block)
     end.inject(&method(:merge))
   end
 
+  # Sort an array utilizing concurrency via multiple processes.
   def self.tort_process_sort(unsorted_array, process_workers = Parallel.processor_count, &block)
     Parallel.map(chunk_array(unsorted_array, process_workers), in_processes: process_workers) do |sub_array|
       sub_array.sort(&block)
     end.inject(&method(:merge))
   end
 
+  # Chunk an array into a list of sub arrays optimal for processing
+  # by the number of workers specified.
   def self.chunk_array(array, workers)
     array.each_slice([(array.size / workers), 1].max)
   end
 
   private_class_method :chunk_array
 
+  # Merge two **sorted** arrays.
   def self.merge(left, right)
     result = []
     result << (left[0] <= right[0] ? left : right).shift until left.empty? || right.empty?
