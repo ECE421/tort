c025451dbbe21d13f96981943787a2f124c06e8d
Thomas Lorincz <tlorincz@ualberta.ca>
2019-03-07 18:18:44 -0700

    Adjusting test array sizes.

--- test/tort_test.rb
+++ test/tort_test.rb
@@ -11,21 +11,21 @@ class TortTest < Test::Unit::TestCase
 
   def test_thread_sort_rand
     unsorted_array = Array.new(10) { rand(-10_000_000...10_000_000) }
-    assert_equal(unsorted_array.sort, Tort.thread_sort(unsorted_array, 10))
+    assert_equal(unsorted_array.sort, Tort.thread_sort(unsorted_array, 1))
   end
 
   def test_thread_sort_timeout
-    unsorted_array = Array.new(100_000) { rand(-10_000_000...10_000_000) }
+    unsorted_array = Array.new(100_000_000) { rand(-10_000_000...10_000_000) }
     assert_raise(TimeoutError) { Tort.thread_sort(unsorted_array, 0.1) }
   end
 
   def test_process_sort_rand
     unsorted_array = Array.new(10) { rand(-10_000_000...10_000_000) }
-    assert_equal(unsorted_array.sort, Tort.process_sort(unsorted_array, 10))
+    assert_equal(unsorted_array.sort, Tort.process_sort(unsorted_array, 1))
   end
 
   def test_process_sort_timeout
-    unsorted_array = Array.new(100_000) { rand(-10_000_000...10_000_000) }
+    unsorted_array = Array.new(100_000_000) { rand(-10_000_000...10_000_000) }
     assert_raise(TimeoutError) { Tort.process_sort(unsorted_array, 0.1) }
   end
 end
