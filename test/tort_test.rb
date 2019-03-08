ffb7ab9681390c7cea1eba122aa4437a80cfeb0e
Thomas Lorincz <tlorincz@ualberta.ca>
2019-03-07 17:01:12 -0700

    Fixed duration test arguments (thought they were milliseconds instead of seconds).

--- test/tort_test.rb
+++ test/tort_test.rb
@@ -11,21 +11,21 @@ class TortTest < Test::Unit::TestCase
 
   def test_thread_sort_rand
     unsorted_array = Array.new(10) { rand(-10_000_000...10_000_000) }
-    assert_equal(unsorted_array.sort, Tort.thread_sort(unsorted_array, 1000))
+    assert_equal(unsorted_array.sort, Tort.thread_sort(unsorted_array, 10))
   end
 
   def test_thread_sort_timeout
     unsorted_array = Array.new(100_000) { rand(-10_000_000...10_000_000) }
-    assert_raise(Parallel::Kill) { Tort.thread_sort(unsorted_array, 10) }
+    assert_raise(Parallel::Kill) { Tort.thread_sort(unsorted_array, 0.1) }
   end
 
   def test_process_sort_rand
     unsorted_array = Array.new(10) { rand(-10_000_000...10_000_000) }
-    assert_equal(unsorted_array.sort, Tort.process_sort(unsorted_array, 1000))
+    assert_equal(unsorted_array.sort, Tort.process_sort(unsorted_array, 10))
   end
 
   def test_process_sort_timeout
     unsorted_array = Array.new(100_000) { rand(-10_000_000...10_000_000) }
-    assert_raise(Parallel::Kill) { Tort.process_sort(unsorted_array, 10) }
+    assert_raise(Parallel::Kill) { Tort.process_sort(unsorted_array, 0.1) }
   end
 end
