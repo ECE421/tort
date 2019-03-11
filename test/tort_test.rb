baf2fd72c0187acb01057ffa66e338a175dc1d5b
Nathan Klapstein <nklapste@ualberta.ca>
2019-03-11 13:03:39 -0600

    improving test grouping

--- test/tort_test.rb
+++ test/tort_test.rb
@@ -9,6 +9,8 @@ class TortTest < Test::Unit::TestCase
     # Do nothing
   end
 
+  # Thread sort tests
+
   def test_thread_sort_rand
     unsorted_array = Array.new(10) { rand(-10_000_000...10_000_000) }
     assert_equal(unsorted_array.sort, Tort.thread_sort(unsorted_array, 1))
@@ -18,6 +20,22 @@ class TortTest < Test::Unit::TestCase
     unsorted_array = Array.new(100_000_000) { rand(-10_000_000...10_000_000) }
     assert_raise(Timeout::Error) { Tort.thread_sort(unsorted_array, 0.1) }
   end
+
+  def test_thread_sort_enum
+    unsorted_enum = Array.new(10) { rand(-10_000_000...10_000_000) }.to_enum
+    assert_equal(unsorted_enum.sort, Tort.thread_sort(unsorted_enum, 1))
+  end
+
+  def test_thread_sort_unsortable
+    assert_raise(ArgumentError) { Tort.thread_sort([1, 'foo'], 10) }
+    assert_raise(ArgumentError) { Tort.thread_sort('foo', 10) }
+  end
+
+  def test_thread_sort_invalid_workers
+    assert_raise(ArgumentError) { Tort.thread_sort([], 10, -1) }
+  end
+
+  # Process sort tests
 
   def test_process_sort_rand
     unsorted_array = Array.new(10) { rand(-10_000_000...10_000_000) }
@@ -29,14 +47,17 @@ class TortTest < Test::Unit::TestCase
     assert_raise(Timeout::Error) { Tort.process_sort(unsorted_array, 0.1) }
   end
 
-  def test_process_sort_unsortable
-    unsorted_array = Array.new(10) { rand(-10_000_000...10_000_000) }
-    unsorted_array.append('foo')
-    assert_raise(ArgumentError) { Tort.process_sort(unsorted_array, 10) }
-  end
-
   def test_process_sort_enum
     unsorted_enum = Array.new(10) { rand(-10_000_000...10_000_000) }.to_enum
-    assert_equal(unsorted_enum.sort, Tort.thread_sort(unsorted_enum, 1))
+    assert_equal(unsorted_enum.sort, Tort.process_sort(unsorted_enum, 1))
+  end
+
+  def test_process_sort_unsortable
+    assert_raise(ArgumentError) { Tort.process_sort([1, 'foo'], 10) }
+    assert_raise(ArgumentError) { Tort.process_sort('foo', 10) }
+  end
+
+  def test_process_sort_invalid_workers
+    assert_raise(ArgumentError) { Tort.process_sort([], 10, -1) }
   end
 end
