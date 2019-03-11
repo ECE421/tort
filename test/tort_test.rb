b8e31f5899c832bed47efe04d70511b74fefc237
Nathan Klapstein <nklapste@ualberta.ca>
2019-03-11 12:50:14 -0600

    adding better support for enum and tests for errors

--- test/tort_test.rb
+++ test/tort_test.rb
@@ -28,4 +28,15 @@ class TortTest < Test::Unit::TestCase
     unsorted_array = Array.new(100_000_000) { rand(-10_000_000...10_000_000) }
     assert_raise(Timeout::Error) { Tort.process_sort(unsorted_array, 0.1) }
   end
+
+  def test_process_sort_unsortable
+    unsorted_array = Array.new(10) { rand(-10_000_000...10_000_000) }
+    unsorted_array.append('foo')
+    assert_raise(ArgumentError) { Tort.process_sort(unsorted_array, 10) }
+  end
+
+  def test_process_sort_enum
+    unsorted_enum = Array.new(10) { rand(-10_000_000...10_000_000) }.to_enum
+    assert_equal(unsorted_enum.sort, Tort.thread_sort(unsorted_enum, 1))
+  end
 end
